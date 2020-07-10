194cf97a426090d903f78c11039e71e6f7838a28
Olivia Helmuth <Olivia.Helmuth@pm.me>
2020-07-10 14:14:43 +0200

    initial commit

---
+++ components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="navbar">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" href="./">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./projects">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+$light: #ebf3ea;
+$light2: #ffffff;
+$green: #9eb294;
+$rose: #e38b9c;
+* {
+  font-family: "Raleway", sans-serif;
+  box-sizing: border-box;
+}
+#navbar {
+  padding: 1rem 0;
+  background-color: $light;
+  a {
+    color: $rose;
+    font-size: 1.8rem;
+    margin: 0 2rem;
+  }
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <div id="main">
+      <div class="jumbotron">
+        <h1 class="display-5">Hello, world!</h1>
+        <p>Former Tech Support and now web developer</p>
+        <hr class="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+      </div>
+      <div id="main2">
+        <div class="jumbotron"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+$light: #ebf3ea;
+$light2: #ffffff;
+$green: #9eb294;
+$rose: #e38b9c;
+#main {
+  background-color: $light;
+  display: flex;
+  align-items: center;
+  height: 90vh;
+  .jumbotron {
+    margin-left: 25rem;
+    background-color: white;
+    border-radius: 30px;
+    z-index: 1;
+  }
+  #main2 {
+    position: relative;
+    .jumbotron {
+      background-color: $rose;
+      height: 28rem;
+      width: 20rem;
+      border-radius: 60px;
+      position: absolute;
+      margin-left: 0;
+      bottom: -13rem;
+      right: -4rem;
+      z-index: 0;
+    }
+  }
+}
+</style>
